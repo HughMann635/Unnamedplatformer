608270a66c6597ed1f7590df178e8ab046517172
HughMann635 <zahrangalib@gmail.com>
2026-07-04 17:17:02 -0400

    add some blank levels, to serve as placeholders

--- src/tilemap.h
+++ src/tilemap.h
@@ -2,8 +2,21 @@
 #include "vars.h"
 #include <string>
 
+//Will fill these in once I actually decide on level layouts lmao
 std::string levels[5][6] = {
     {
-    
+        "", "", "", "", "", ""
+    } ,
+    {
+        "", "", "", "", "", ""
+    } ,
+    {
+        "", "", "", "", "", ""
+    } ,
+    {
+        "", "", "", "", "", ""
+    } ,
+    {
+        "", "", "", "", "", ""
     }
 };
