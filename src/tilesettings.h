dfdb347206b2bcae7c78bd50d8841842b45fd29e
HughMann635 <zahrangalib@gmail.com>
2026-07-06 13:59:53 -0400

    set up enum class for tile types

---
+++ src/tilesettings.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "vars.h"
+
+enum class tiletype {
+    //Basic tile types
+    empty,
+    spawn,
+    exit,
+    solid,
+    //Obstacles / non-player entities
+    spike,
+    doublespike,
+    pushblock,
+    spring,
+    blackhole,
+    //Environments
+    lava,
+    water,
+    zero_g
+};
