e52f2732a5804d14ce080df5f798fa85892f05d6
HughMann635 <zahrangalib@gmail.com>
2026-07-06 14:46:07 -0400

    set down the foundation for loading the maps

--- src/tilesettings.h
+++ src/tilesettings.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include "vars.h"
+#include "tilemap.h"
 
 enum class tiletype {
     //Basic tile types
@@ -28,5 +29,16 @@ struct Tile {
 class tilemap {
 public:
     std::vector<Tile> tilelist;
-    
+    sf::Vector2f spawn;
+    sf::Vector2f finish;
+
+    void loadmap (std::string level) {
+        for (int i = 0; i < 36; i++) {
+            for (int j = 0; j < 64; j++) {
+                char tilecode = level[i][j];
+                Tile new_tile;
+                new_tile.tile.setSize(sf::Vector2f(playerdim, playerdim));
+            }
+        }
+    }
 };
