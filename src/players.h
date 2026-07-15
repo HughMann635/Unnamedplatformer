bc07180e0fe6f8b743844774b0d5e2e21e2c0254
HughMann635 <zahrangalib@gmail.com>
2026-07-15 17:45:38 -0400

    reorganize players.h into players.h and players.cpp because I had to avoid circular include issues with my swept aabb implementation

--- src/players.h
+++ src/players.h
@@ -34,281 +34,52 @@ class square: public player {
 public:
     sf::RectangleShape playershape;
 
-    square() {
-        playershape = sf::RectangleShape(sf::Vector2f(playerdim, playerdim));
-	    playershape.setFillColor(sf::Color::Red);
-        
-        velocity = (sf::Vector2f(0.f, gravity));
-        grounded = false;
-    }
-
-    sf::Shape& shape() override {
-        return playershape;
-    }
-
-    void jump (float deltatime) override {
-        if (sf::Keyboard::isKeyPressed ( sf::Keyboard::Key::Up) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::W)) {
-            if (grounded && !swimming && !zerogactive) {
-                velocity.y -= jumpforce;
-                grounded = false;
-            }
-        }
-        if (swimming) {
-            if (sf::Keyboard::isKeyPressed (sf::Keyboard::Key::Down) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::S)) {
-                velocity.y += 50.f;
-                grounded = false;
-            }
-            if (sf::Keyboard::isKeyPressed (sf::Keyboard::Key::Up) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::W)) {
-                velocity.y = -125.f;
-                grounded = false;
-            }
-        } 
-    }
-
-    void updatepos (float deltatime, tilemap& map) override {
-        if (sf::Keyboard::isKeyPressed (sf::Keyboard::Key::Right) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::D)) {
-            if (velocity.x > movespeed) velocity.x -= circleaccel*0.7;
-            else velocity.x = movespeed;
-		} else if (sf::Keyboard::isKeyPressed (sf::Keyboard::Key::Left) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::A)) {
-            if (velocity.x < -movespeed) velocity.x += circleaccel*0.7;
-            else velocity.x = -movespeed;
-		} else {
-            zerogactive || swimming ? velocity.x *= 0.8 : velocity.x *= 0.f; 
-        }
-        if (zerogactive) {
-            if (sf::Keyboard::isKeyPressed (sf::Keyboard::Key::Down) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::S)) {
-                velocity.y = 125.f;
-            }
-            else if (sf::Keyboard::isKeyPressed (sf::Keyboard::Key::Up) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::W)) {
-                velocity.y = -125.f;
-            }
-            else {
-                velocity.y *= 0.96;
-            }
-        } 
-
-        if (swimming) {
-            gravity = 250.f; 
-        } else if (zerogactive) {
-            gravity = 0.f;
-        } else if (!swimming && !zerogactive) { 
-            gravity = 1800.f;
-        }
-
-        moveobject(deltatime, gravity);
-
-        sf::Vector2f bound = playershape.getPosition();
-        bound.x = std::clamp(bound.x, 0.f, (float)width-playerdim);
-        playershape.setPosition(bound);
-        swimming = false;
-        zerogactive = false;
-    }
-
-    void drawscreen (sf::RenderWindow& window) override {
-        window.draw(playershape);
-    }
+    square();
+    sf::Shape& shape() override;
+    void jump (float deltatime) override;
+    void updatepos (float deltatime, tilemap& map) override;
+    void drawscreen (sf::RenderWindow& window) override;
 };
 
 class circle : public player {
 public:
     sf::CircleShape playershape;
 
-    circle() {
-        playershape = sf::CircleShape(playerdim/2);
-	    playershape.setFillColor(sf::Color(0, 255, 255));
-
-        velocity = (sf::Vector2f(0.f, gravity));
-        grounded = false;
-    }
-
-    sf::Shape& shape() override {
-        return playershape;
-    }
-
-    void jump (float deltatime) override {
-        if (sf::Keyboard::isKeyPressed ( sf::Keyboard::Key::Up) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::W)) {
-            if (grounded && !swimming && !zerogactive) {
-                velocity.y -= jumpforce;
-                grounded = false;
-            }
-        }
-        if (swimming) {
-            if (sf::Keyboard::isKeyPressed (sf::Keyboard::Key::Down) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::S)) {
-                velocity.y += 50.f;
-                grounded = false;
-            }
-            if (sf::Keyboard::isKeyPressed (sf::Keyboard::Key::Up) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::W)) {
-                velocity.y = -125.f;
-                grounded = false;
-            }
-        } 
-    }
-
-    void updatepos (float deltatime, tilemap& map) override {
-        if (sf::Keyboard::isKeyPressed (sf::Keyboard::Key::Right) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::D)) {
-            if (velocity.x <= circlemax && !zerogactive) velocity.x += circleaccel;
-            else if (zerogactive) velocity.x = movespeed;
-		} else if (sf::Keyboard::isKeyPressed (sf::Keyboard::Key::Left) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::A)) {
-            if (velocity.x >= -circlemax && !zerogactive) velocity.x += -circleaccel;
-            else if (zerogactive) velocity.x = -movespeed;
-		} else if (swimming || zerogactive) {
-            velocity.x *= 0.8; 
-        } else {
-            if (velocity.x > 0) velocity.x -= circleaccel*0.7;
-            else if (velocity.x < 0) velocity.x += circleaccel*0.7;
-            else velocity.x = 0;
-        }
-        if (zerogactive) {
-            if (sf::Keyboard::isKeyPressed (sf::Keyboard::Key::Down) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::S)) {
-                velocity.y = 125.f;
-            }
-            else if (sf::Keyboard::isKeyPressed (sf::Keyboard::Key::Up) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::W)) {
-                velocity.y = -125.f;
-            }
-            else {
-                velocity.y *= 0.96;
-            }
-        } 
-
-        if (swimming) {
-            gravity = 250.f; 
-        } else if (zerogactive) {
-            gravity = 0.f;
-        } else if (!swimming && !zerogactive) { 
-            gravity = 1800.f;
-        }
-
-        moveobject(deltatime, gravity);
-
-        sf::Vector2f bound = playershape.getPosition();
-        bound.x = std::clamp(bound.x, 0.f, (float)width-playerdim);
-        playershape.setPosition(bound);
-        swimming = false;
-        zerogactive = false;
-    }
-
-    void drawscreen (sf::RenderWindow& window) override {
-        window.draw(playershape);
-    }
+    circle();
+    sf::Shape& shape() override;
+    void jump (float deltatime) override;
+    void updatepos (float deltatime, tilemap& map) override;
+    void drawscreen (sf::RenderWindow& window) override;
 };
 
 class triangle : public player {
 public:
     sf::ConvexShape playershape;
 
-    triangle() {
-        playershape.setPointCount(3);
-        playershape.setPoint(0, sf::Vector2f(0, 17.3));
-        playershape.setPoint(1, sf::Vector2f(10, 0));
-        playershape.setPoint(2, sf::Vector2f(20, 17.3));
-	    playershape.setFillColor(sf::Color(0, 255, 0));
+    triangle();
+    sf::Shape& shape() override;
+    void jump (float deltatime) override;
+    void updatepos (float deltatime, tilemap& map) override;
+    void drawscreen (sf::RenderWindow& window) override;
 
-        velocity = (sf::Vector2f(0.f, gravity));
-        grounded = false;
-    }
-
-    sf::Shape& shape() override {
-        return playershape;
-    }
-
-    void jump (float deltatime) override {
-        if (sf::Keyboard::isKeyPressed ( sf::Keyboard::Key::Up) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::W)) {
-            if (grounded && !swimming && !zerogactive) {
-                velocity.y -= jumpforce;
-                grounded = false;
-            }
-        }
-        if (swimming) {
-            if (sf::Keyboard::isKeyPressed (sf::Keyboard::Key::Down) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::S)) {
-                velocity.y += 50.f;
-                grounded = false;
-            }
-            if (sf::Keyboard::isKeyPressed (sf::Keyboard::Key::Up) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::W)) {
-                velocity.y = -125.f;
-                grounded = false;
-            }
-        } 
-    }
-
-    void updatepos (float deltatime, tilemap& map) override {
-        if (sf::Keyboard::isKeyPressed (sf::Keyboard::Key::Right) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::D)) {
-            if (velocity.x > movespeed) velocity.x -= circleaccel*0.7;
-            else velocity.x = movespeed;
-		} else if (sf::Keyboard::isKeyPressed (sf::Keyboard::Key::Left) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::A)) {
-            if (velocity.x < -movespeed) velocity.x += circleaccel*0.7;
-            else velocity.x = -movespeed;
-		} else {
-            zerogactive || swimming ? velocity.x *= 0.8 : velocity.x *= 0.f; 
-        }
-
-        if (tp_timer.getElapsedTime().asSeconds() >= 3.f) {
-            if ((sf::Keyboard::isKeyPressed (sf::Keyboard::Key::LShift) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::RShift))) {
-                if (sf::Keyboard::isKeyPressed (sf::Keyboard::Key::Right) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::D)) {
-                    if (map.predictCollision(sf::FloatRect(shape().getPosition(), sf::Vector2f(40.f, 0.f)))) {
-                        playershape.setPosition(sf::Vector2f(playershape.getPosition().x + 40, playershape.getPosition().y));
-                        velocity = sf::Vector2f(0, 0);
-                        tp_timer.restart();
-                    }
-                } else if (sf::Keyboard::isKeyPressed (sf::Keyboard::Key::Left) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::A)) {
-                    if (map.predictCollision(sf::FloatRect(shape().getPosition(), sf::Vector2f(40.f, 0.f)))) {
-                        playershape.setPosition(sf::Vector2f(playershape.getPosition().x - 40, playershape.getPosition().y));
-                        velocity = sf::Vector2f(0, 0);
-                        tp_timer.restart();
-                    }
-                } else if (sf::Keyboard::isKeyPressed (sf::Keyboard::Key::Up) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::W)) {
-                    if (map.predictCollision(sf::FloatRect(shape().getPosition(), sf::Vector2f(40.f, 0.f)))) {                        
-                        playershape.setPosition(sf::Vector2f(playershape.getPosition().x, playershape.getPosition().y - 40));
-                        velocity = sf::Vector2f(0, 0);
-                        tp_timer.restart();
-                    }
-                } else if (sf::Keyboard::isKeyPressed (sf::Keyboard::Key::Down) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::S)) {
-                    if (map.predictCollision(sf::FloatRect(shape().getPosition(), sf::Vector2f(40.f, 0.f)))) {
-                        playershape.setPosition(sf::Vector2f(playershape.getPosition().x, playershape.getPosition().y + 40));
-                        velocity = sf::Vector2f(0, 0);
-                        tp_timer.restart();
-                    }
-                }
-            }
-        }
-
-        if (zerogactive) {
-            if (sf::Keyboard::isKeyPressed (sf::Keyboard::Key::Down) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::S)) {
-                velocity.y = 125.f;
-            }
-            else if (sf::Keyboard::isKeyPressed (sf::Keyboard::Key::Up) || sf::Keyboard::isKeyPressed (sf::Keyboard::Key::W)) {
-                velocity.y = -125.f;
-            }
-            else {
-                velocity.y *= 0.96;
-            }
-        } 
-
-        if (swimming) {
-            gravity = 250.f; 
-        } else if (zerogactive) {
-            gravity = 0.f;
-        } else if (!swimming && !zerogactive) { 
-            gravity = 1800.f;
-        }
-
-        moveobject(deltatime, gravity);
-
-        sf::Vector2f bound = playershape.getPosition();
-        bound.x = std::clamp(bound.x, 0.f, (float)width-playerdim);
-        playershape.setPosition(bound);
-        swimming = false;
-        zerogactive = false;
-    }
-
-    void drawscreen (sf::RenderWindow& window) override {
-        window.draw(playershape);
-    }
 };
 
 class hexagon : public player {
+    sf::ConvexShape playershape;
 
+    hexagon();
+    sf::Shape& shape() override;
+    void jump (float deltatime) override;
+    void updatepos (float deltatime, tilemap& map) override;
+    void drawscreen (sf::RenderWindow& window) override;
 };
 
 class octagon : public player {
-    
+    sf::ConvexShape playershape;
+
+    octagon();
+    sf::Shape& shape() override;
+    void jump (float deltatime) override;
+    void updatepos (float deltatime, tilemap& map) override;
+    void drawscreen (sf::RenderWindow& window) override;
 };
