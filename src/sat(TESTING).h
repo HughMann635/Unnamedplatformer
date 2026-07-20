2bbf4caa17d6ec73ee5c2394e910f6d3b5255e96
HughMann635 <zahrangalib@gmail.com>
2026-07-20 12:41:20 -0400

    finish drawdebug() function which draws axes and vertices of objects

--- src/sat(TESTING).h
+++ src/sat(TESTING).h
@@ -11,13 +11,12 @@ std::vector<sf::Vector2f> getvertices(sf::Shape& shape) {
     return vertices;
 }
 
-std::vector<sf::Vector2f> getaxes(sf::Shape& shape) {
-    std::vector<sf::Vector2f> vertices = getvertices(shape);
+std::vector<sf::Vector2f> getaxes(std::vector<sf::Vector2f> vertices) {
     std::vector<sf::Vector2f> axes;
-    for (int i = 0; i < shape.getPointCount(); i++) {
-        sf::Vector2f edge = vertices[i+1] - vertices[i];
+    for (int i = 0; i < vertices.size(); i++) {
+        sf::Vector2f edge = vertices[(i+1) % vertices.size()] - vertices[i];
         sf::Vector2f normal (-edge.y, edge.x); 
-        float length = sqrt(normal.x * normal.x + normal.y * normal.x);
+        float length = sqrt(normal.x * normal.x + normal.y * normal.y);
         axes.push_back(normal / length);
     }
     return axes;
@@ -29,5 +28,21 @@ void drawdebug(sf::RenderWindow& window, std::vector<sf::Vector2f> vertices) {
         vertex.setRadius(3.f);
         vertex.setPosition(sf::Vector2f(pos.x, pos.y));
         window.draw(vertex);
+    }
+    std::vector<sf::Vector2f> axes = getaxes(vertices);
+    for (int i = 0; i < vertices.size(); i++) {
+        sf::Vector2f pt1 = vertices[i];
+        sf::Vector2f pt2 = vertices[(i+1) % vertices.size()];
+        sf::Vector2f midpoint = (pt1+pt2)/2.f;
+        sf::Vector2f axis = midpoint + axes[i] * 25.f;
+        sf::Vertex v1;
+        v1.position = midpoint;
+        v1.color = sf::Color::Magenta;
+        sf::Vertex v2;
+        v1.position = axis;
+        v1.color = sf::Color::Magenta;
+
+        sf::Vertex axisline[2] = { v1, v2 };
+        window.draw(axisline, 2, sf::PrimitiveType::Lines);
     }
 }
