595aa90c5a5f70a0b7fea831c2fc452bcab3c900
HughMann635 <zahrangalib@gmail.com>
2026-07-29 16:31:52 -0400

    implement multiple checkCollisions iterations to properly solve stacked objects however, it causes insane lag so I will have to look into a way to solve that

--- src/main.cpp
+++ src/main.cpp
@@ -77,6 +77,8 @@ int main()
 		map.checkCollisions(*currentplayer);
 		map.updatemap(deltatime);
 		map.checkCollisions(*currentplayer);
+		map.checkCollisions(*currentplayer);
+		map.checkCollisions(*currentplayer);
 		currentplayer -> rotateobject(edge, map, currentplayer -> shape(), deltatime, movespeed, swimming, zerogactive, currentplayer -> grounded, nearestedge);
 		lastframe_pos = sf::Vector2f(currentplayer -> shape().getPosition());
 		lastframe_vel = sf::Vector2f(currentplayer -> velocity);
