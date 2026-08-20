0d2658e9c5fe2aed4abd19530906464287b367e7
HughMann635 <zahrangalib@gmail.com>
2026-08-20 13:58:01 -0400

    playtest levels 1-28 and make minor adjustments to remove unwanted exploits and make some levels more playable

--- src/vars.h
+++ src/vars.h
@@ -52,8 +52,8 @@ inline bool obstacletop = false;
 inline bool obstaclebelow = false;
 
 //Game settings
-inline int levelnum = 5;
-inline int setnum = 0;
+inline int levelnum = 2;
+inline int setnum = 4;
 inline bool circlelocked = true;
 inline bool trianglelocked = true;
 inline bool octagonlocked = true;
