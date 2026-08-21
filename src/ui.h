2958c2033cb7e46d2bc59ab0c4e5c3c7406030c4
HughMann635 <zahrangalib@gmail.com>
2026-08-21 19:49:39 -0400

    add mainmenu constructor and set up title and start button

--- src/ui.h
+++ src/ui.h
@@ -6,7 +6,28 @@
 #include "states.h"
 
 class mainmenu {
+public:
+    sf::Font font;
+    sf::Text title;
+    sf::Text startbtn;
+    
+    mainmenu() :
+        title(font),
+        startbtn(font)
+    {
+        font.openFromFile("AldotheApache.ttf");
+        title.setFont(font);
+        title.setString("WORLDS OF BLOCKMAN");
+        title.setCharacterSize(55);
+        title.setFillColor(sf::Color(170, 100, 255));
+        title.setPosition(sf::Vector2f(width/2-(title.getGlobalBounds().size.x), 100));
 
+        startbtn.setFont(font);
+        startbtn.setString("PLAY");
+        startbtn.setCharacterSize(35);
+        startbtn.setFillColor(sf::Color(170, 100, 255));
+        startbtn.setPosition(sf::Vector2f(width/2-(title.getGlobalBounds().size.x), 100));
+    }
 };
 
 class playing {
