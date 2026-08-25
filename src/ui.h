11f662f276869f4f4f449ef2cfd8dfdfce558549
HughMann635 <zahrangalib@gmail.com>
2026-08-25 13:28:18 -0400

    add a rectangular button for main menu instead of just text and center it

--- src/ui.h
+++ src/ui.h
@@ -9,12 +9,13 @@ class mainmenu {
 public:
     sf::Font font;
     sf::Text title;
-    sf::Text startbtn;
+    sf::Text starttxt;
+    sf::RectangleShape startbtn;
     
     mainmenu() :
         font("AldotheApache.ttf"),
         title(font),
-        startbtn(font)
+        starttxt(font)
     {
         title.setFont(font);
         title.setString("WORLDS OF BLOCKMAN");
@@ -22,27 +23,41 @@ public:
         title.setFillColor(sf::Color(170, 100, 255));
         title.setPosition(sf::Vector2f((width-title.getGlobalBounds().size.x)/2, 100));
 
-        startbtn.setFont(font);
-        startbtn.setString("SPACE TO PLAY, ESC FROM GAME TO QUIT"); //WILL MAKE INTO A CLICKABLE BUTTON
-        startbtn.setCharacterSize(35);
-        startbtn.setFillColor(sf::Color(170, 100, 255));
-        startbtn.setPosition(sf::Vector2f((width-startbtn.getGlobalBounds().size.x)/2, 500));
+        starttxt.setFont(font);
+        starttxt.setString("SPACE TO PLAY, ESC FROM GAME TO QUIT");
+        starttxt.setCharacterSize(35);
+        starttxt.setFillColor(sf::Color(170, 100, 255));
+        starttxt.setOrigin(sf::Vector2f(starttxt.getLocalBounds().position.x + starttxt.getLocalBounds().size.x/2, starttxt.getLocalBounds().position.y + starttxt.getLocalBounds().size.y/2));
+        starttxt.setPosition(sf::Vector2f(width/2, 450));
+
+        startbtn.setSize(sf::Vector2f(starttxt.getLocalBounds().size.x*1.2, starttxt.getLocalBounds().size.y*2));
+        startbtn.setFillColor(sf::Color(100, 30, 155));
+        startbtn.setOrigin(sf::Vector2f(startbtn.getLocalBounds().size.x/2, startbtn.getLocalBounds().size.y/2));
+        startbtn.setPosition(sf::Vector2f(width/2, starttxt.getPosition().y));
     }
 
     void play (sf::RenderWindow& window) {
         sf::Vector2i mouse_ = sf::Mouse::getPosition(window);
         sf::Vector2f mousepos = sf::Vector2f((float)mouse_.x, (float)mouse_.y);
-        if (startbtn.getGlobalBounds().contains(mousepos)) {
-            startbtn.setFillColor(sf::Color(255, 170, 30));
+        if (starttxt.getGlobalBounds().contains(mousepos)) {
+            starttxt.setFillColor(sf::Color(255, 170, 30));
             if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) state = State::playing;
         }
-        else startbtn.setFillColor(sf::Color(170, 100, 255));
+        else starttxt.setFillColor(sf::Color(170, 100, 255));
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space)) state = State::playing;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Enter)) std::cout <<
+            "\nbutton origin: " << startbtn.getOrigin().x << ", " << startbtn.getOrigin().y <<
+            "\ntext origin: " << starttxt.getOrigin().x << ", " << starttxt.getOrigin().y <<
+            "\nbutton size: " << startbtn.getSize().x << ", " << startbtn.getSize().y <<
+            "\ntext size: " << starttxt.getGlobalBounds().size.x << ", " << starttxt.getGlobalBounds().size.y <<
+            "\nbutton pos: " << startbtn.getPosition().x << ", " << startbtn.getPosition().y <<
+            "\ntext pos: " << starttxt.getPosition().x << ", " << starttxt.getPosition().y;
     }
 
     void draw (sf::RenderWindow& window) {
         window.draw(title);
         window.draw(startbtn);
+        window.draw(starttxt);
     }
 };
 
@@ -77,14 +92,14 @@ class pause {
 public:
     sf::Font font;
     sf::Text paused;
-    sf::Text resumebtn;
-    sf::Text exitbtn;
+    sf::Text resumetxt;
+    sf::Text exittxt;
 
     pause() :
         font("AldotheApache.ttf"),
         paused(font),
-        resumebtn(font),
-        exitbtn(font)
+        resumetxt(font),
+        exittxt(font)
     {
         paused.setFont(font);
         paused.setString("PAUSED");
@@ -92,36 +107,36 @@ public:
         paused.setCharacterSize(35);
         paused.setPosition(sf::Vector2f((width-paused.getGlobalBounds().size.x)/2, 340));
 
-        resumebtn.setFont(font);
-        resumebtn.setString("SPACE TO RESUME");
-        resumebtn.setFillColor(sf::Color(80, 210, 145));
-        resumebtn.setCharacterSize(25);
-        resumebtn.setPosition(sf::Vector2f((width-resumebtn.getGlobalBounds().size.x)/2, 440));
+        resumetxt.setFont(font);
+        resumetxt.setString("SPACE TO RESUME");
+        resumetxt.setFillColor(sf::Color(80, 210, 145));
+        resumetxt.setCharacterSize(25);
+        resumetxt.setPosition(sf::Vector2f((width-resumetxt.getGlobalBounds().size.x)/2, 440));
 
-        exitbtn.setFont(font);
-        exitbtn.setString("ESCAPE TO EXIT");
-        exitbtn.setFillColor(sf::Color(145, 80, 210));
-        exitbtn.setCharacterSize(25);
-        exitbtn.setPosition(sf::Vector2f((width-exitbtn.getGlobalBounds().size.x)/2, 490));
+        exittxt.setFont(font);
+        exittxt.setString("ESCAPE TO EXIT");
+        exittxt.setFillColor(sf::Color(145, 80, 210));
+        exittxt.setCharacterSize(25);
+        exittxt.setPosition(sf::Vector2f((width-exittxt.getGlobalBounds().size.x)/2, 490));
     }
 
     void draw (sf::RenderWindow& window) {
         window.draw(paused);
-        window.draw(resumebtn);
-        window.draw(exitbtn);
+        window.draw(resumetxt);
+        window.draw(exittxt);
     }
 
     void checkaction(sf::RenderWindow& window) {
         sf::Vector2i mouse_ = sf::Mouse::getPosition(window);
         sf::Vector2f mousepos = sf::Vector2f((float)mouse_.x, (float)mouse_.y);
-        if (resumebtn.getGlobalBounds().contains(mousepos)) {
-            resumebtn.setFillColor(sf::Color(180, 200, 20));
+        if (resumetxt.getGlobalBounds().contains(mousepos)) {
+            resumetxt.setFillColor(sf::Color(180, 200, 20));
             if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) state = State::playing;
-        } else resumebtn.setFillColor(sf::Color(80, 210, 145));
-        if (exitbtn.getGlobalBounds().contains(mousepos)) {
-            exitbtn.setFillColor(sf::Color(20, 120, 255));
+        } else resumetxt.setFillColor(sf::Color(80, 210, 145));
+        if (exittxt.getGlobalBounds().contains(mousepos)) {
+            exittxt.setFillColor(sf::Color(20, 120, 255));
             if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) state = State::mainmenu;
-        } else exitbtn.setFillColor(sf::Color(145, 80, 210));
+        } else exittxt.setFillColor(sf::Color(145, 80, 210));
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space)) state = State::playing;
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape)) state = State::mainmenu; 
     }
